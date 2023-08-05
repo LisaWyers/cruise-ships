bdbe482fafa2ee8bfaaa6063481242660cd58df3
LisaWyers <134444159+LisaWyers@users.noreply.github.com>
2023-08-05 10:56:03 +0100

    adding final js files

--- src/Port.js
+++ src/Port.js
@@ -1,6 +1,17 @@
 function Port(name) {
-this.currentName = name;
+  this.currentName = name;
+  this.ships = [];
 }
 
+Port.prototype.addShip = function (ship) {
+  this.ships.push(ship);
+};
+
+Port.prototype.removeShip = function (ship) {
+  const shipIndex = this.ships.indexOf(ship);
+  if (shipIndex !== -1) {
+    this.ships.splice(shipIndex, 1);
+  }
+};
 
 module.exports = Port;
